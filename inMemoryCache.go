8b7df537dee609668b4f75e08d97ab8ca93dccc9
Vasiliy Siunov <vasiunov@gmail.com>
2023-03-10 22:23:04 +0300

    fix readme. Add mutex and time-to-live

--- inMemoryCache.go
+++ inMemoryCache.go
@@ -1,28 +1,48 @@
 package cache
 
-// Cache type contains the field map[string]interface{}.
+import (
+	"sync"
+	"time"
+)
+
+// Cache type contains map[string]interface{}, time-to-live and mutex fields.
 type Cache struct {
-	c map[string]interface{}
+	c   map[string]interface{}
+	ttl time.Duration
+	mu  *sync.Mutex
 }
 
-// Function NewCache() initialise Cache struct.
+// Function NewCache() initialise Cache struct. Each key-value pair expires in 5 sec.
 func NewCache() Cache {
 	return Cache{
-		c: make(map[string]interface{}),
+		c:   make(map[string]interface{}),
+		ttl: time.Second * 5,
+		mu:  new(sync.Mutex),
 	}
 }
 
-// Method Set() sets the interface{} value for a string key.
+// Method Set() sets the interface{} value for a string key. Method contains goroutine deleting data according to time-to-live.
 func (c Cache) Set(key string, value interface{}) {
+	c.mu.Lock()
 	c.c[key] = value
+	c.mu.Unlock()
+
+	go func() {
+		time.Sleep(c.ttl)
+		c.Delete(key)
+	}()
 }
 
 // Method Get() returns the interface{} value corresponding to a string key.
 func (c Cache) Get(key string) interface{} {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	return c.c[key]
 }
 
-// Method Delete() deletes the key-interface{} pair.
+// Method Delete() deletes the key-interface{} pair. Use Delete() if you don't need to wait for time-to-live to expire.
 func (c Cache) Delete(key string) {
+	c.mu.Lock()
 	delete(c.c, key)
+	c.mu.Unlock()
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
-Package cache is used for caching data.
+Package cache is used for caching data. Each key-value pair expires in 5 sec.
 Description is available here - https://pkg.go.dev/github.com/vasiunov/in-memory-cache
+Play code - https://go.dev/play/p/dBzJ5Uv-Kjc
 
 ## Example
 
@@ -12,19 +13,23 @@ import (
 )
 
 func main() {
+	someCache := NewCache()
 
-	someCache := cache.NewCache()
-	fmt.Println(someCache) // {map[]}
+	for i := 0; i <= 10; i++ {
+		time.Sleep(10 * time.Millisecond)
+		go someCache.Set(fmt.Sprintf("%d", i), i*i)
+	}
 
-	someCache.Set("userID", 59)
-	fmt.Println(someCache) // {map[userID:59]}
+	for i := 0; i <= 10; i++ {
+		time.Sleep(10 * time.Millisecond)
+		go fmt.Println(someCache.Get(fmt.Sprintf("%d", i))) // 0 1 4 9 16 25 36 49 64 81 100
+	}
 
-	userId := someCache.Get("userID")
-	fmt.Println(userId) // 59
+	time.Sleep(time.Second * 3)
+	fmt.Println("2sec left", someCache) // 2sec left {map[0:0 1:1 10:100 2:4 3:9 4:16 5:25 6:36 7:49 8:64 9:81] 5000000000 0xc0000b2000}
 
-	someCache.Delete("userID")
-	userId = someCache.Get("userID")
-
-	fmt.Println(userId) // <nil>
+	time.Sleep(time.Second * 3)
+	fmt.Println(someCache.Get("1"), "time is out") // <nil> time is out
+	fmt.Println(someCache) // {map[] 5000000000 0xc0000b2000}
 }
 ```
